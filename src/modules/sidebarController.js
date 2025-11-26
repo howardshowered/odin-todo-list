17d8c044b9883f1d4a47ee79c7eab16b38da13ef
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-11-26 14:51:48 -0800

    worked on SidebarController module render function

--- src/modules/sidebarController.js
+++ src/modules/sidebarController.js
@@ -1,6 +1,57 @@
+import ProjectController from "./projectController";
 const SidebarController = ( () => {
 
-    const asideElement = document.querySelector('aside');
+    const sidebar = document.querySelector('aside');
+    const addTask = document.querySelector('.add-nav');
+
+
+    const render = () => {
+        sidebar.textContent = "";
+        
+        const header = document.createElement("header");
+
+        const icon = document.createElement("img");
+        icon.src = "./res/honeydew.svg";
+        icon.className = "honey-dew-icon";
+
+        const title = document.createElement("h1");
+        title.textContent = "2Dew";
+
+        header.appendChild(icon);
+        header.appendChild(title);
+
+        const nav = document.querySelector("nav");
+        const addNav = document.createElement("div");
+        addNav.className = "add-nav";
+
+        const addIcon = document.createElement("img");
+        addIcon.src = "./res/todo-add-svgrepo-com.svg";
+        addIcon.className = "add-icon";
+        const addTaskH1 = document.createElement("h1");
+        addTaskH1.textContent = "Add Task";
+        addNav.appendChild(addIcon);
+        addNav.appendChild(addTaskH1);
+
+        nav.appendChild(addNav);
+
+
+        //Create Project items 
+        const projects = ProjectController.getProjects();
+        for ( let project of projects) {
+            
+        }
+
+
+
+    }
+
+
+
+
+
     
 
-})
+
+    
+
+})();
