c982b26640f7535050e44072bb8809cd2e63c411
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-12-03 14:09:38 -0800

    add task button added to main content

--- src/modules/contentController.js
+++ src/modules/contentController.js
@@ -4,6 +4,12 @@ const ContentController = (() => {
     const main = document.querySelector("main");
     const projectTitle = document.createElement("h1");
     main.appendChild(projectTitle);
+    const addTaskBtn = document.createElement("button");
+    addTaskBtn.className = "add-task-btn";
+    addTaskBtn.textContent = "Add Task";
+    main.appendChild(addTaskBtn);
+    addTaskBtn.hidden = true;
+
 
     let activeProject = null;
 
@@ -13,6 +19,7 @@ const ContentController = (() => {
         activeProject = project;
         main.innerHtml = "";
         projectTitle.textContent = activeProject.getName();
+         addTaskBtn.hidden = false;
 
     }
 
